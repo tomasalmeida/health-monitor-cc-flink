fa39696b12d862ccb86b2301db14da3cfe80c17d
Tomás Dias Almeida <tomas.almeida@gmail.com>
2026-01-19 06:30:30 -0300

    idea for the CSE day

--- flink_statements.tf
+++ flink_statements.tf
@@ -1,11 +1,20 @@
 # --------------------------------------------------------
-# Step 1: Create a connection to read patient data from the source databased
+# Step 1: Create a connection to read patient data from the source databased - tomas
 # --------------------------------------------------------
 
 # --------------------------------------------------------
-# Step 2: Separate the events in their own topics by type
+# Step 2: remove anomalies from health events (ML_DETECT_ANOMALIES)  - paolo - heart_rate
 # --------------------------------------------------------
 
 # --------------------------------------------------------
-# Step 3: remove anomalies from health events
+# Step 3: identifying exercise vs heart attack (ML_DETECT_ANOMALIES_ROBUST) 
 # --------------------------------------------------------
+
+# --------------------------------------------------------
+# Step 4: push alerts to tableflow
+# --------------------------------------------------------
+
+# --------------------------------------------------------
+# Step 5: Improve the data random generator to be more predictable
+# --------------------------------------------------------
+
